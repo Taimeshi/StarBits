f65e3a76c6d76143a785a21a70b717609e6311fd
小川　大心 <taishinchoco@gmail.com>
2022-09-26 14:03:29 +0900

    Default Changelist

--- star_trailblazer.py
+++ star_trailblazer.py
@@ -20,6 +20,8 @@ def main():
     # 変数
     plots = []
     longs = []
+    scroll = 0
+    pixel_per_measure = 600
     
     def load_from_snp():
         pass
@@ -39,6 +41,15 @@ def main():
                 pg.quit()
                 sys.exit()
 
+        plots_poped_after = []
+        for p_i, p in enumerate(plots):
+            distance = (p[2] + p[4] / p[3]) * pixel_per_measure
+            note = rs.graphic(NOTE_IMAGES)[p[0]]
+            p_y = 700 + scroll - distance - note.get_height() / 2
+            if -127 < p_y < 700 + 127:
+                sc.blit(note,
+                        [150 + p[1] * 100 - note.get_width() / 2 + 50, 700 + scroll - distance - note.get_height() / 2])
+        
         pg.display.update()
         cl.tick(60)
 
